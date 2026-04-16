3360ece52fb84f7532d6da6793fca05be5e0f979
candravero <candravero395@gmail.com>
2026-04-16 10:59:10 +0300

    Create main.py file with ASCII art and initialization code

---
+++ main.py
@@ -0,0 +1,26 @@
+# main.py
+
+"""
+This is the main entry point of the application.
+"""
+
+# ASCII Art
+ascii_art = r"""
+    _   __        _       
+   / | / /  ____| |      
+   | |/ /  / __ \ |      
+   |   /  | |  | | |     
+   | |\ \ | |  | | |     
+   |_| \_\ \____/|_|     
+"""
+
+print(ascii_art)
+
+# Initialization code
+
+def initialize():
+    print("Initializing the application...")
+    # Add your initialization code here
+
+if __name__ == '__main__':
+    initialize()
